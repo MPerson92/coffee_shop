c4e17240aeecf93bf6cc504fcc75940eae01a9e9
MPerson92 <76800220+MPerson92@users.noreply.github.com>
2021-03-24 16:16:44 -0400

    updated coffee shop

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var eatsDrinks = document.getElementById("eatsDrinks")
+eatsDrinks.classList.add("eatsDrinks");
+const add = document.getElementById("button");
+add.addEventListener("click", event => {
+    var coffee = document.getElementById("num1").value;
+   
+    var tea = document.getElementById("num2").value;
+    
+    var bagel = document.getElementById("num3").value;
+    
+    var result = document.getElementById("result")
+    result.textContent = "Price: " + "$" + ((tea * 7) + (coffee * 11) + (bagel * 3));
+
+})  
+
+button.classList.add();
+
+
 
